241936f7945e12ed5e5bfb60bf6904df456620e9
Paul-Julien Vauthier <pj82491@gmail.com>
2017-02-16 20:53:04 +0800

    Use Spring Boot, serve static files, docker config

--- src/main/java/com/javaquarium/Application.java
+++ src/main/java/com/javaquarium/Application.java
@@ -3,12 +3,11 @@ package com.javaquarium;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by Administrator on 2016/8/20.
- */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
+
+
 }
